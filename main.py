5b587f0429ab9532da6f33318f06a1c52d0dd15e
Udit Joshi <udit.joshi@hbwsl.com>
2026-06-01 12:01:50 +0530

    feature: dropped all columns except for the required ones, and exported as data1.xlsx

--- main.py
+++ main.py
@@ -6,7 +6,17 @@ def load_data(path: str):
     df = pd.read_csv(path)
     return df
 
+def process_data(df: pd.DataFrame):
+    required_cols = ['sku', 'name', 'short_description', 'description', 'categories', 'additional_attributes']
+    df = df[required_cols]
+    df.to_excel('data1.xlsx', index=False)  # Save the data inside of excel file with disabled numerical index (serial number types)
+    return df
+
 if __name__ == "__main__":
     csv_path = 'export_catalog_product_20251006_105554.csv'
     data = load_data(csv_path)
-    print(data.head())
+    print("Data loaded successfully.")
+    print(data.head(), end = "\n\n")
+    processed_data = process_data(data)
+    print("Data has been processed and saved to data1.xlsx")
+    print(processed_data.head(), end = "\n\n")
